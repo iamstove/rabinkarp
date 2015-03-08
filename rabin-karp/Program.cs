a239a3134803cacea7adb1df1fde48857be37d2e
Steven Turner <sturner1123@gmail.com>
2015-03-08 14:54:36 -0400

    finished horner's method on the input

--- rabin-karp/Program.cs
+++ rabin-karp/Program.cs
@@ -11,19 +11,30 @@ namespace rabin_karp
     {
         static void Main(string[] args)
         {
+            int R = 256; //size of ascii
             Console.WriteLine("Welcome to Rabin Karp!");
+            string path = Directory.GetCurrentDirectory();
+            //Console.WriteLine(path);
             Console.Write("Please enter the name of a text file you want to search through: ");
             string fname = Console.ReadLine();
-            string path = Directory.GetCurrentDirectory();
+            path += "/" + fname;
+            //Console.WriteLine(path);
+            Console.Write("Enter what you're searching for: ");
+            string pattern = Console.ReadLine();
+            pattern.Trim(); //we won't want to search for leading or trailing whitespace
+            //hash the pattern
+            double pathorners = 0;
+            for(int i = 0; i < (int)pattern.Length; i++){ //use horner's method on the pattern to create a hash for the string
+                Console.WriteLine("R ^ (len-i) = " + Math.Pow(R, ((int)pattern.Length - i)));
+                Console.WriteLine("char = " + (int)pattern[i]);
+                double letter = (double)((int)pattern[i] * Math.Pow(R,((int)pattern.Length - i)));
+                //Console.WriteLine(letter);
+                pathorners += letter;
+            }
 
-            using(FileStream fs = File.Open(path, FileMode.Open, FileAccess.Read){ //do everything with the file in here, in this case it's everything
-                Console.Write("Enter what you're searching for: ");
-                string pattern = Console.ReadLine();
-                //hash the pattern
-                for(int i = 0; i < (int)pattern.Length; i++){
-
-                }
+            using(FileStream fs = File.Open(path, FileMode.Open, FileAccess.Read)){ //do everything with the file in here, in this case it's everything
                 //first read is size pattern.length, everything after is just 1 to compute the rolling hash. 
+
             }
         }
     }
