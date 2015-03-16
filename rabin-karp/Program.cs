580b58a5c93c00e6864d86299458f3a62c46e67a
unknown <SRT31@PH5308-DESK03.univ.pitt.edu>
2015-03-16 13:46:47 -0400

    reset

--- rabin-karp/Program.cs
+++ rabin-karp/Program.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Numerics;
 
+
 namespace rabin_karp
 {
     class Program
@@ -23,9 +24,9 @@ namespace rabin_karp
             pattern.Trim(); //we won't want to search for leading or trailing whitespace
             //hash the pattern
             BigInteger pathorners = horners(pattern);
-
-            using(FileStream fs = File.Open(path, FileMode.Open, FileAccess.Read)){ //do everything with the file in here, in this case it's everything
-                //first read is size pattern.length, everything after is just 1 to compute the rolling hash. 
+            using (FileStream fs = File.Open(path, FileMode.Open, FileAccess.Read))
+            { //do everything with the file in here, in this case it's everything
+                //first read is size pattern.length, everything after is just 1 to compute the rolling hash.
                 string subtext = "";
                 int counter = 0;
                 for (int i = 0; i < (int)pattern.Length; i++)
@@ -33,37 +34,9 @@ namespace rabin_karp
                     subtext += (char)fs.ReadByte();
                     counter++;
                 } //subtext is now ready to be hashed
-<<<<<<< HEAD
                 BigInteger txthorners = horners(subtext);
-                Console.WriteLine(txthorners);
-                if (txthorners == pathorners)
+                do
                 {
-                    Console.WriteLine("Match found at location " + counter);
-                    Environment.Exit(0);
-                }
-                else //no match, we must roll it baby
-                {
-                    while (txthorners != pathorners)
-                    {
-                        char next = (char)fs.ReadByte(); //read in the new byte
-                        counter++;
-                        if ((int)next == -1)
-                        {
-                            Console.WriteLine("Pattern not found");
-                            break;
-                        }
-                        //Console.Write();
-                        txthorners = rollhash(txthorners, subtext[0], next, (int)subtext.Length); //roll the hash
-                        subtext = subtext.Remove(0, 1); //change the string in the text
-                        subtext += next;
-                        //BigInteger txthorners2 = horners(subtext);
-                        Console.WriteLine(subtext + " = " + txthorners);
-                        //System.Threading.Thread.Sleep(500);
-                    }
-=======
-                BigInteger txthorners = horners(subtext);                
-                do{ 
->>>>>>> c55e6e17adbfeb79e341472e530aad7077b2e320
                     if (txthorners == pathorners)
                     {
                         Console.WriteLine("Match found at location " + counter);
@@ -72,35 +45,32 @@ namespace rabin_karp
                     int next = fs.ReadByte(); //read in the new byte
                     counter++;
                     if (next == -1)
-                    {                            
+                    {
                         Console.WriteLine("End of file reached");
                         break;
                     }
                     txthorners = rollhash(txthorners, subtext[0], (char)next, (int)subtext.Length); //roll the hash
                     subtext = subtext.Remove(0, 1); //change the string in the text
                     subtext += (char)next;
-
                     //Console.WriteLine(subtext + " = " + txthorners);
-                    //System.Threading.Thread.Sleep(500);                    
-                }while(true);
+                    //System.Threading.Thread.Sleep(500);
+                } while (true);
             }
             Console.Read();
         }
-
         public static BigInteger horners(string instring)
         {
             BigInteger hhash = 0;
             for (int i = 0; i < (int)instring.Length; i++)
             { //use horner's method on the pattern to create a hash for the string
-                BigInteger letter = (BigInteger)((int)instring[i] * Math.Pow(R, ((int)instring.Length - (i+1))));
+                BigInteger letter = (BigInteger)((int)instring[i] * Math.Pow(R, ((int)instring.Length - (i + 1))));
                 hhash += letter;
             }
             return hhash;
         }
-
         public static BigInteger rollhash(BigInteger hash, char oldchar, char newchar, int len) //removes the front of the oldchar from the front and appends newchar to the end
         {
-            BigInteger rollnum = (BigInteger)((int)oldchar * Math.Pow(R, len-1));
+            BigInteger rollnum = (BigInteger)((int)oldchar * Math.Pow(R, len - 1));
             hash = hash - rollnum;
             hash = hash * R;
             hash = hash + (int)newchar;
